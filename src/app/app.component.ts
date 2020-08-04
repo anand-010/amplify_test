8e30429999c2da42d683ce693e88ca03835d67b2
anand <anands9288907423@gmail.com>
2020-08-04 18:32:47 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { APIService } from './API.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-amplify';
+  constructor(private apiService: APIService){
+    setTimeout(() => {
+      apiService.CreateTodo({name:"angular", description:"testing" , address:"my addresss"}).then((result)=>{
+        console.log("result", result);
+        
+      })
+    }, 5000);
+
+    apiService.OnCreateTodoListener.subscribe((val)=>{
+      console.log("value", val);
+      
+    })
+    apiService.OnaddtodoByIdListener.subscribe(val=>{
+      console.log('listener with id', val);
+      
+    })
+
+  }
 }
